f05ba40517cee648596bb6dea8aacdf54c2615fd
Smak2025 <smak-80+2025@yandex.ru>
2025-11-24 13:56:18 +0300

    Добавьте файлы проекта.

---
+++ Threads1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Threads1;
+
+var n = 2_000_000_000;
+var sequentialSummator = new SequentialSummator(n);
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+sequentialSummator.CalcSum();
+stopwatch.Stop();
+Console.WriteLine("{0} за {1} мс.", sequentialSummator.Result, stopwatch.ElapsedMilliseconds);
+
+var parallelSummator = new ParallelSummator(n);
+stopwatch.Restart();
+parallelSummator.CalcSum();
+stopwatch.Stop();
+Console.WriteLine("{0} за {1} мс.", parallelSummator.Result, stopwatch.ElapsedMilliseconds);
---
+++ Threads1/SequentialSummator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threads1
+{
+    public class SequentialSummator
+    {
+        public int N
+        {
+            get => field;
+            set => field = Math.Abs(value);
+        }
+
+        public long Result
+        {
+            get => field;
+            private set => field = value;
+        }
+
+        public SequentialSummator(int n)
+        {
+            N = n;
+        }
+
+        public void CalcSum()
+        {
+            for (int i = 1; i <= N; i++)
+            {
+                Result += i;
+            }
+        }
+    }
+}
